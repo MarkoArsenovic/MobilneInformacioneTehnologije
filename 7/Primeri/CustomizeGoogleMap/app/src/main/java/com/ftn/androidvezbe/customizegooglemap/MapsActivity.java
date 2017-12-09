b4a60eb35a3e05251c07cb686f5c9535134b7202
MarkoArsenovic <arsenovic.marko91@gmail.com>
2017-12-09 10:00:39 +0100

    Session 7 added

---
+++ 7/Primeri/CustomizeGoogleMap/app/src/main/java/com/ftn/androidvezbe/customizegooglemap/MapsActivity.java
@@ -0,0 +1,59 @@
+package com.ftn.androidvezbe.customizegooglemap;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng noviSad = new LatLng( 45.251667, 19.836944);
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(noviSad)
+                .radius(10000)
+                .strokeColor(Color.RED));
+                //.fillColor(Color.BLUE));
+
+        mMap.addMarker(new MarkerOptions().position(noviSad).title("Novi Sad").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(noviSad, 10));
+    }
+}
